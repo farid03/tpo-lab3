3c52bcb6febd6e2886bebff17d6bb13945606b5f
Farid Kurbanov <f.kurbanov@tinkoff.ru>
2023-05-07 22:34:16 +0300

    selenoid v1

--- src/test/java/ru/ifmo/se/tpolab3/core/BaseSeleniumTest.java
+++ src/test/java/ru/ifmo/se/tpolab3/core/BaseSeleniumTest.java
@@ -6,20 +6,50 @@ import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
+import java.lang.module.Configuration;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.concurrent.TimeUnit;
 
 abstract public class BaseSeleniumTest {
     protected WebDriver driver;
 
     @BeforeEach
-    public void setUp() {
+    public void setUp() throws MalformedURLException {
         WebDriverManager.chromedriver().setup();
-        var chromeOptions = new ChromeOptions();
-        chromeOptions.addArguments("--start-maximized");
-        chromeOptions.addArguments("--remote-allow-origins=*");
+        var options = new ChromeOptions();
+        options.addArguments("--start-maximized");
+        options.addArguments("--remote-allow-origins=*");
 
-        driver = new ChromeDriver(chromeOptions);
+        options.setCapability("browserVersion", "latest");
+        options.setCapability("selenoid:options", new HashMap<String, Object>() {{
+            /* How to add test badge */
+            put("name", "Test badge...");
+
+            /* How to set session timeout */
+            put("sessionTimeout", "15m");
+
+            /* How to set timezone */
+            put("env", new ArrayList<String>() {{
+                add("TZ=UTC");
+            }});
+
+            /* How to add "trash" button */
+            put("labels", new HashMap<String, Object>() {{
+                put("manual", "true");
+            }});
+
+            /* How to enable video recording */
+            put("enableVideo", true);
+
+            put("enableVNC", true);
+        }});
+        driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), options);
+
         driver.manage().window().maximize();
         driver.manage().timeouts().pageLoadTimeout(10, TimeUnit.SECONDS);
         driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
