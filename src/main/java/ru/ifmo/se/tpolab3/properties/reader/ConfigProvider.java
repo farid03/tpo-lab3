970fb0159038be138b15fb0f14982b8ad09053e9
Farid Kurbanov <f.kurbanov@tinkoff.ru>
2023-05-15 00:50:11 +0300

    refactor

--- src/main/java/ru/ifmo/se/tpolab3/properties/reader/ConfigProvider.java
+++ src/main/java/ru/ifmo/se/tpolab3/properties/reader/ConfigProvider.java
@@ -8,6 +8,11 @@ public interface ConfigProvider {
     String MAIN_PAGE_URL = readConfig().getString("pages.main.url");
     String BROWSER_EXTENSION_URL = readConfig().getString("pages.extension.url");
     String NEWS_PAGE_URL = readConfig().getString("pages.news.url");
+    String TEST_MAIN_NEWS_PAGE_URL = readConfig().getString("pages.news.mainNewsPageTestUrl");
+    String MAPS_PAGE_URL = readConfig().getString("pages.maps.url");
+    String MAPS_WIND_PAGE_URL = readConfig().getString("pages.maps.menu.wind");
+    String INFORMER_PAGE_URL = readConfig().getString("pages.informers.url");
+
 
     static Config readConfig() {
         return ConfigFactory.systemProperties().hasPath("testProfile")
